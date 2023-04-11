7be86e6d27a2a743969614472bb4281d6e10c6b1
uma-dev <22565959+uma-dev@users.noreply.github.com>
2023-04-10 20:26:59 -0600

    Added some endpoints and rest controller

---
+++ src/main/java/com/umadev/springboot/demo/app/rest/MyRestController.java
@@ -0,0 +1,27 @@
+package com.umadev.springboot.demo.app.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRestController {
+    
+    // Expose "/" that return "Hello world"
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+    // Expose a new endpoint for "workout"
+
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5k!";
+    }
+
+    // Expose a new endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky day";
+    }
+}
